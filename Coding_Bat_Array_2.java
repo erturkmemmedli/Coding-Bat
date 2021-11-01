5be4c2197f311e471c89a09947c271523611773a
erturkmemmedli <49902768+erturkmemmedli@users.noreply.github.com>
2021-11-01 16:30:07 +0400

    Update Coding_Bat_Array_2.java

--- Coding_Bat_Array_2.java
+++ Coding_Bat_Array_2.java
@@ -222,3 +222,179 @@ public boolean twoTwo(int[] nums) {
   }
   return stuff;
 }
+
+/* sameEnds */
+public boolean sameEnds(int[] nums, int len) {
+  int[] front = Arrays.copyOfRange(nums,0,len);
+  int[] end = Arrays.copyOfRange(nums,nums.length-len,nums.length);
+  if (Arrays.equals(front,end)) return true;
+  return false;
+}
+
+/* tripleUp */
+public boolean tripleUp(int[] nums) {
+  for (int i=0; i<nums.length-1; i++) {
+    if (i>0 && nums[i]-nums[i-1]==1 && nums[i+1]-nums[i]==1)
+      return true;
+  }
+  return false;
+}
+
+/* fizzArray3 */
+public int[] fizzArray3(int start, int end) {
+  int n = end-start;
+  int[] array = new int[n];
+  int count = start;
+  for (int i=0;i<n;i++) {
+    array[i] = count;
+    count++;
+  }
+  return array;
+}
+
+/* shiftLeft */
+public int[] shiftLeft(int[] nums) {
+  int[] arr = new int[nums.length];
+  if (nums.length==0) return nums;
+  arr[nums.length-1] = nums[0];
+  for (int i=0; i<nums.length-1; i++) {
+    arr[i] = nums[i+1];
+  }
+  return arr;
+}
+
+/* tenRun */
+public int[] tenRun(int[] nums) {
+  int[] arr = new int[nums.length];
+  if (nums.length==0 || nums.length==1) arr = nums;
+  for (int i=1; i<nums.length; i++) {
+    arr[0] = nums[0];
+    arr[i] = nums[i];
+    if (arr[i-1]%10==0 && nums[i]%10!=0) arr[i] = arr[i-1];
+  }
+  return arr; 
+}
+
+/* pre4 */
+public int[] pre4(int[] nums) {
+  int i = 0;
+  int index = 0;
+  while (i < nums.length) {
+    if (nums[i] == 4) {
+      index = i;
+      break;
+    }
+    else i++;
+  }
+  return Arrays.copyOfRange(nums,0,index);
+}
+
+/* post4 */
+public int[] post4(int[] nums) {
+  int i = nums.length-1;
+  int index = 0;
+  while (i > 0) {
+    if (nums[i] == 4) {
+      index = i;
+      break;
+    }
+    i--;
+  }
+  return Arrays.copyOfRange(nums,index+1,nums.length);
+}
+
+/* notAlone */
+public int[] notAlone(int[] nums, int val) {
+  int[] arr = nums;
+  for (int i=1; i<nums.length-1; i++) {
+    if (nums[i] == val) {
+      if (nums[i]!=nums[i-1] && nums[i]!=nums[i+1]) {
+        if (nums[i-1] < nums[i+1]) arr[i] = nums[i+1];
+        else arr[i] = nums[i-1];
+      }
+    }
+  }
+  return arr;
+}
+
+/* zeroFront */
+public int[] zeroFront(int[] nums) {
+  int count = 0;
+  int temp = 0;
+  for(int i=0; i<nums.length; i++) {
+    if (nums[i]==0) count++;
+  }
+  int[] arr = new int[nums.length];
+  for (int j=0; j<nums.length; j++) {
+    if (j<count) arr[j] = 0;
+    else {
+      for(int k=0; k<nums.length; k++) {
+        if (nums[k]!=0) temp = nums[k];
+      } 
+      arr[j] = temp;
+    }
+  }
+  return arr;
+}
+
+/* withoutTen */
+public int[] withoutTen(int[] nums) {
+  int[] arr = new int[nums.length];
+  int count = 0;
+  int n = 0;  
+  for(int i=0; i<nums.length; i++) {
+    if (nums[i]!=10) count++;
+  }  
+  for (int j=0; j<nums.length; j++) {
+    if (n < count && nums[j]!=10) {
+      arr[n] = nums[j];
+      n++;
+    }
+  }
+  if (n>=count && n<nums.length) {
+    arr[n] = 0;
+    n++;
+  }
+  return arr;
+}
+
+/* zeroMax */
+public int[] zeroMax(int[] nums) {
+  int max = 0;
+  for (int i=nums.length-1; i>=0; i--) {
+    if (nums[i]%2==1) max = Math.max(max,nums[i]);
+    if (nums[i]==0) nums[i] = max;
+  }
+  return nums;
+}
+
+/* evenOdd */
+public int[] evenOdd(int[] nums) {
+  int count = 0;
+  int even = 0;
+  int odd = 0;
+  for (int i=0; i<nums.length; i++) {
+    if (nums[i]%2==0) {
+      count++;
+      even = nums[i];
+    }
+    else odd = nums[i];
+  }
+  for (int j=0; j<nums.length; j++) {
+    if (j<count) nums[j] = even;
+    else nums[j] = odd;
+  }
+  return nums;
+}
+
+/* fizzBuzz */
+public String[] fizzBuzz(int start, int end) {
+  String[] arr = new String[end - start];
+  for (int i=start; i<end; i++) {
+    if (i%5!=0 && i%3!=0) arr[i-start] = String.valueOf(i);
+    if (i%3==0 && i%5!=0) arr[i-start] = "Fizz";
+    if (i%5==0 && i%3!=0) arr[i-start] = "Buzz";
+    if (i%15==0) arr[i-start] = "FizzBuzz";
+  }
+  return arr;
+}
